f73ff916a61667b6ef7a2c6f99e046ab345cb67c
Lime <66lime7@protonmail.com>
2026-08-15 14:38:49 +0200

    feat(llm): add PyTorch token and position embeddings

---
+++ pureml/llm/torchgpt/embeddings.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+
+class TokenPositionEmbedding(nn.Module):
+    def __init__(self, vocab_size: int, ctx_length: int, embedding_dim: int):
+        super().__init__()
+        self.context_length = ctx_length
+
+        self.token_embedding_layer = nn.Embedding(vocab_size, embedding_dim)
+        self.pos_embedding_layer = nn.Embedding(
+            ctx_length,
+            embedding_dim,
+        )
+        # init the weights as before
+        nn.init.normal_(self.token_embedding_layer.weight, mean=0.0, std=0.02)
+        nn.init.normal_(self.pos_embedding_layer.weight, mean=0.0, std=0.01)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        if token_ids.ndim != 2:
+            raise ValueError("token_ids must have shape (batch_size, sequence_length)")
+
+        batch_size, sequence_length = token_ids.shape
+        # positions = np.broadcast_to(
+        #     np.arange(sequence_length),
+        #     (batch_size, sequence_length),
+        # )
+        positions = torch.arange(sequence_length, device=token_ids.device)
+        token_embeddings = self.token_embedding_layer(token_ids)
+        pos_embeddings = self.pos_embedding_layer(positions)
+        # unsqueeze adds dimension of 1 at pos 0
+        return token_embeddings + pos_embeddings.unsqueeze(0)
